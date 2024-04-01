ea8818184b2dc73d5836d5e1fc87d667d011eea0
suizzle <andygolay@gmail.com>
2024-04-01 10:44:03 -0400

    Fix consent form

--- js-extensions/packages/consent-form/lib/index.tsx
+++ js-extensions/packages/consent-form/lib/index.tsx
@@ -52,10 +52,3 @@ let ConsentForm = () => {
     </div>
   );
 };
-
-if (localStorage.getItem(CONSENT_KEY) === null) {
-  let el = document.createElement("div");
-  document.body.appendChild(el);
-  let root = ReactDOM.createRoot(el);
-  root.render(<ConsentForm />);
-}
